676abe0157a32bf487d33f190b4f016762c264d4
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-11-08 20:18:29 +0000

    Created createTimesTable function

--- project/script.js
+++ project/script.js
@@ -1,1 +1,21 @@
-console.log(`test`)
+//User input elements
+const userInput = {
+    btnSubmit: document.querySelector("#btn-submit"),
+    numInput: document.querySelector("#multi-num-input"),
+};
+
+const timeTable = []
+
+//Creates array of mutiples
+function createTimesTable(mutiNumInput, startPos, endPos){
+    startPos = startPos -1;
+    const numOfPostions = endPos - startPos;
+    let mutiNum = mutiNumInput * startPos;
+
+    for(let i = 0; i < numOfPostions; i++){
+        mutiNum = mutiNum + mutiNumInput;
+        timeTable[i] = mutiNum;
+    };
+};
+
+createTimesTable(5, 1, 10);
