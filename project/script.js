d70b289e7b65e8b42485a838f58736c3c26d863a
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-11-09 18:30:05 +0000

    Added functionality to createTimesTable allowing multiple tables to be calculated Created functionality within "createTimesTable" 	-Allowing mulitple values to be passed 	-Calcualte each passed values multiplication table 	-Creates and returns an array of arrays 		-Each postion containing a single values multiplication table
    Global Scope Changes
    	-Created "numberInputs" array
    		-Will store mutiple values input by the user
    		-Currently stores hard coded test values
    
    	-"timeTable" declaration
    		-Stores "createTimesTable" return result
    
    "createTimesTable" Function Changes
    	-Creates and returns each passed values times table
    
    	-"multiNumInput" name changed "numberInputs"
    
    	-Created "multiplyNumbers" Function
    		-Created 2 paramters
    			-"multiplyNum"
    				-Value used to create mutiplications
    			-"numOfPositions"
    				-Defines loop execution number
    
    		-Created 2 variables
    			-"singleTimeTable" array
    				-Stores all multiplication values
    			-"positionNum"
    				-Used to itterate through "mutiplyNum" mutiplications
    
    		-for loop
    			-Inserts "positionNum" into "singleTimeTable[i]"
    			-Adds "multiplyNum" value to "positionNum" value
    
    		-Function returns "singleTimeTable" array
    
    	-Created for in "numberInputs" loop
    		-Decalres "multiNum"
    			-"multiNum" = "numberInputs[currentLoopExecution]"
    			-Passed into "mutiplyNumbers" function
    		-"timeTable[currentLoopExecution]" = "multiplyNumbers" function result
    
    	-Returns "timeTable"

--- project/script.js
+++ project/script.js
@@ -1,21 +1,40 @@
 //User input elements
 const userInput = {
     btnSubmit: document.querySelector("#btn-submit"),
-    numInput: document.querySelector("#multi-num-input"),
+    numInput: document.querySelector("#in-multi-num"),
+    startInput: document.querySelector("#in-pos-start"),
+    endInput: document.querySelector("#in-pos-end"),
 };
 
-const timeTable = []
+const numberInputs = [38,5,4564];
 
-//Creates array of mutiples
-function createTimesTable(mutiNumInput, startPos, endPos){
+//Creates an array of mutiplication tables
+function createTimesTable(numberInputs, startPos, endPos){
     startPos = startPos -1;
     const numOfPostions = endPos - startPos;
-    let mutiNum = mutiNumInput * startPos;
+    let timeTable = [];
+    
+    //Creates multiples of multiplyNum value 
+    function multiplyNumbers(multiplyNum, numOfPostions){
+        let singleTimeTable = [];
+        let positionNum = multiplyNum;
 
-    for(let i = 0; i < numOfPostions; i++){
-        mutiNum = mutiNum + mutiNumInput;
-        timeTable[i] = mutiNum;
+        for(let i = 0; i < numOfPostions; i++){
+            singleTimeTable[i] = positionNum;
+            positionNum = positionNum + multiplyNum
+        };
+
+        return singleTimeTable;
     };
+
+    //Inserts individual number mutiplication tables into timeTable - Runs for each input
+    for(const num in numberInputs){
+        let multiNum = numberInputs[num];
+        timeTable[num] = multiplyNumbers(multiNum, numOfPostions); 
+    };
+
+    return timeTable;
 };
 
-createTimesTable(5, 1, 10);
+const timeTable = createTimesTable(numberInputs, 1, 10);
+console.log(timeTable)
